e974e672f08ba49e1f72db9b3646d148942da55a
brenner8023 <vue666@qq.com>
2022-10-23 23:19:06 +0800

    feat: add vite-plugin-static-copy

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,21 @@ import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: './node_modules/@idux/components/icon/svg/*.svg',
+          dest: 'idux-icons',
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
